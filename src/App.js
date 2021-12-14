0ee1c10009720bb9bf22eebc73d491fdd5c4d697
shubham21699 <shubham21699@gmail.com>
2021-12-14 23:37:03 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { CssBaseline, Grid } from '@material-ui/core';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
@@ -9,9 +10,18 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <List />
-      <Map />
-      <PlaceDetails />
+      <CssBaseline />
+      <Grid container spacing={3} style={{ width: '100%' }} > 
+          {/* This item xs={12} means it will take full width on mobile devices */}
+          {/* and md={4} means it will take 4 spaces on medium or large devices */}
+          <Grid item xs={12} md={4}> 
+            <List />
+          </Grid>
+          {/* Map is larger so md={8} will take 8 spaces on medium or larger devices */}
+          <Grid item xs={12} md={8} >
+            <Map />
+          </Grid>
+       </Grid>
     </div>
   );
 }
